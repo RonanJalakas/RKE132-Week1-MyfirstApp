0cf7e1fcf436f6e4b3e96206f0ec28395c5ed051
RonanJalakas <ronan.jalakas@tktk.ee>
2024-05-04 10:45:32 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name:"); //Output
+        string userName;
+        userName = Console.ReadLine(); //Input
+        Console.WriteLine("Hello" + userName + "!"); //Output
+        Console.WriteLine($"Hello, {userName} !"); //Output
+    }
+}
